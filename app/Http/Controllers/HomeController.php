75193c65f62b0db49df8241ead9736f6227200f7
Aryo <chiefdevica@gmail.com>
2023-03-21 16:19:13 +0700

    add update praktikum 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,8 +27,9 @@ class HomeController extends Controller
     //     return view('home');
     // }
 
-    public function index(){
+    public function index()
+    {
         $user = Auth::user();
-        return view('home',['user'=>$user]);
+        return view('index2',['user'=>$user]);
     }
 }
